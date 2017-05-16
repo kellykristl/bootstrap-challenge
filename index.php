0be7869fd4a91a75db35d39b586f06b9b87ccd29
kellykristl <goddesswebdesign@gmail.com>
2017-05-16 08:51:03 -0600

    added flexbox sticky footer and bootstrap dependencies

--- index.php
+++ index.php
@@ -2,8 +2,32 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- My Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<script src="js/bootstrap.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<title>Bootstrap Challenge</title>
 	</head>
 	<body>
-		<title>Bootstrap Challenge</title>
-	</body>
+		<body class="sfooter">
+
+			<div class="sfooter-content">
+				<!-- your page content here-->
+			</div>
+
+			<footer>
+				<!-- footer here-->
+				placeholder text for footer
+			</footer>
+		</body>
 </html>
